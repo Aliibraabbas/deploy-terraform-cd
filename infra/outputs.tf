49f1411c6c73ec446b447c88a33e717ed7968a7f
Ali ABBAS <ali.ibra.abbas@gmail.com>
2025-07-31 11:51:55 +0200

    feat(terraform): expose instance public IP and DynamoDB table outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,15 @@
 output "cloudfront_distribution_domain_name" {
   value = module.cdn.cloudfront_distribution_domain_name
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+}
+
+output "security_group_id" {
+  value = aws_security_group.ecs_tasks.id
+}
